7aee357ab9a17f379507fba588ce1dd47c026fd7
betysr <betysr96@gmail.com>
2022-11-01 20:22:55 +0100

    some changes done

--- Week3/homework/Ex3/sql-injection.js
+++ Week3/homework/Ex3/sql-injection.js
@@ -7,6 +7,7 @@ var con = createConnection({
   database: "world",
 });
 
+// sql injection happens
 function getPopulation(Country, name, code, cb) {
   con.query(
     `SELECT Population FROM ${Country} WHERE Name = '${name}' and code = '${code}'`,
@@ -16,25 +17,27 @@ function getPopulation(Country, name, code, cb) {
       cb(null, result);
     }
   );
-	//THIS IS THE RIGHT VERSION
-	/* 
-	con.query(
-    `SELECT Population FROM ? WHERE Name = ? and code = ?`,
-    [Country, name, code],
+}
+
+// correct function
+function getPopulationCorrected(Country, name, code, cb) {
+  name = con.escape(name);
+  code = con.escape(code);
+  con.query(
+    `SELECT Population FROM ${Country} WHERE Name = ${name} and code = ${code}`,
     function (err, result) {
       if (err) cb(err);
       if (result.length == 0) cb(new Error("Not found"));
       cb(null, result);
     }
   );
-	*/
 }
 
-const res = (error, result) => {
+// to see the correct one, change function name as getPopulationCorrected
+getPopulation("country", "Aruba", "ABW 'OR' 1=1 ;", (error,result)=>{
   if (error) {
     console.log(error);
   }
   console.table(result);
-};
-getPopulation("country", "Aruba", "ABW ' OR ' 1=1 ;  ", res);
+});
 con.end();
